e5f5f2a6410bfb982bcba8bd302a01bc523eaf03
maoxianxian <c7ye@UCSD.EDU>
2018-02-15 13:21:47 -0800

    finish init

---
+++ New Unity Project/Assets/initColliders.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class initColliders : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MeshCollider[] meshes = this.GetComponentsInChildren<MeshCollider> (); 
+		foreach (MeshCollider m in meshes) {
+			m.convex = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
