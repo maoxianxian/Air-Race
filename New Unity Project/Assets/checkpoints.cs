14ad80fa683aaa0c15a40dc32cbfcfa6a986c243
maoxianxian <c7ye@UCSD.EDU>
2018-02-15 16:35:07 -0800

    integrate leapmotion

--- New Unity Project/Assets/checkpoints.cs
+++ New Unity Project/Assets/checkpoints.cs
@@ -5,14 +5,20 @@ using System.IO;
 public class checkpoints : MonoBehaviour {
 	public TextAsset T;
 	System.IO.StreamReader reader;
+	public GameObject sphere;
+	GameObject campus;
 	// Use this for initialization
 	void Start () {
+		campus = GameObject.FindGameObjectWithTag ("campus");
 		reader = new StreamReader (toStream (T.text));
 		string[] data;
 		while (!reader.EndOfStream) {
 			data = reader.ReadLine ().Split (' '); 
-			foreach (string s in data) {
-			}
+			GameObject temp=GameObject.Instantiate (sphere);
+			Debug.Log (temp);
+			temp.transform.parent = campus.transform;
+			temp.transform.position = new Vector3 (float.Parse(data [0])*0.0254f, float.Parse(data [1])*0.0254f, float.Parse(data [2])*0.0254f);
+			//temp.transform.localScale = new Vector3 (0.0254f, 0.0254f, 0.0254f);
 		}
 	}
 	
