f0fff23ef70170cceeec624577caab2a05e858b5
maoxianxian <c7ye@UCSD.EDU>
2018-02-16 15:53:58 -0800

    hands

--- New Unity Project/Assets/checkpoints.cs
+++ New Unity Project/Assets/checkpoints.cs
@@ -7,26 +7,58 @@ public class checkpoints : MonoBehaviour {
 	System.IO.StreamReader reader;
 	public GameObject sphere;
 	GameObject campus;
+	Leap.Hand lefthand;
+	Leap.Hand righthand;
+	Leap.Controller controller;
+	GameObject player;
 	// Use this for initialization
 	void Start () {
+		controller= new Leap.Controller ();
 		campus = GameObject.FindGameObjectWithTag ("campus");
 		reader = new StreamReader (toStream (T.text));
 		string[] data;
 		while (!reader.EndOfStream) {
 			data = reader.ReadLine ().Split (' '); 
 			GameObject temp=GameObject.Instantiate (sphere);
-			Debug.Log (temp);
 			temp.transform.parent = campus.transform;
 			temp.transform.position = new Vector3 (float.Parse(data [0])*0.0254f, float.Parse(data [1])*0.0254f, float.Parse(data [2])*0.0254f);
-			//temp.transform.localScale = new Vector3 (0.0254f, 0.0254f, 0.0254f);
 		}
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		connectToHands ();
+		if (lefthand != null) {
+			
+		}
 	}
 
+	void connectToHands(){
+		if (controller.Frame ().Hands.Count == 1) {
+			Leap.Hand temp = controller.Frame ().Hands [0];
+			if (temp.IsLeft) {
+				lefthand = temp;
+				righthand = null;
+			} else {
+				righthand = temp;
+				lefthand = null;
+			}
+		} else if (controller.Frame ().Hands.Count == 2) {
+			Leap.Hand temp = controller.Frame ().Hands [0];
+			if (temp.IsLeft) {
+				lefthand = temp;
+				righthand = controller.Frame ().Hands [1];
+			} else {
+				righthand = temp;
+				lefthand = controller.Frame ().Hands [1];
+			}
+
+		} else {
+			lefthand = null;
+			righthand = null;
+		}
+	}
 	Stream toStream(string str)
 	{
 		MemoryStream stream = new MemoryStream ();
